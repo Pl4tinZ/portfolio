f25f2f155e4f4645def99a4a5f43e58bc2d9f09e
Pl4tinZ <109986183+Pl4tinZ@users.noreply.github.com>
2023-01-25 13:32:32 +0100

    added scroll steps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HeaderComponent},
+  {path: 'start-screen', component: StartScreenComponent},
+  {path: 'my-skills', component: MySkillsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
