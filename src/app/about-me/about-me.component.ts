0f2bf6903c42f7b6d19333f0fe9b7fde0a10c9a6
Pl4tinZ <Dennisfrese1@web.de>
2023-10-20 09:16:19 +0200

    add scumble effect

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef} from '@angular/core';
 import { LanguageService } from '../services/language.service';
+
+declare var IntersectionObserver: {
+  prototype: IntersectionObserver;
+  new(callback: IntersectionObserverCallback, options?: IntersectionObserverInit): IntersectionObserver;
+};
 
 @Component({
   selector: 'app-about-me',
@@ -8,6 +13,81 @@ import { LanguageService } from '../services/language.service';
 })
 export class AboutMeComponent {
 
-  constructor(public languageservice: LanguageService) {}
+  private chars: string = '!<>-_\\/[]{}—=+*^?#________';
+  private frame: number = 0;
+  private queues: any[] = [];
+  private frameRequests: number[] = [];
+  private resolves: Function[] = [];
 
+  constructor(public languageservice: LanguageService, private elRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.languageservice.checkSession();
+  }
+
+  ngAfterViewInit(): void {
+    const elements = this.elRef.nativeElement.querySelectorAll('.scramble');
+    elements.forEach((element, index) => {
+      const observer = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting) {
+          this.startScramble(element, index).then(() => {
+          });
+          observer.disconnect();
+        }
+      });
+  
+      observer.observe(element);
+    });
+  }
+
+  startScramble(element: HTMLElement, index: number): Promise<void> {
+    return new Promise(resolve => {
+      const oldText = element.innerText;
+      const length = oldText.length;
+  
+      this.resolves[index] = resolve;
+      this.frame = 0;
+      this.queues[index] = [];
+      this.queueChars(oldText, index);
+  
+      this.update(index);
+    });
+  }
+
+  queueChars(text: string, index: number): void {
+    for (let i = 0; i < text.length; i++) {
+      const char = text[i];
+      const end = this.frame + Math.floor(Math.random() * 200);
+      this.queues[index].push({ char, end });
+    }
+  }
+
+  update(index: number): void {
+    let output = '';
+    let complete = 0;
+  
+    for (let i = 0, n = this.queues[index].length; i < n; i++) {
+      const { char, end } = this.queues[index][i];
+  
+      if (this.frame >= end) {
+        complete++;
+        output += char;
+      } else {
+        output += this.randomChar();
+      }
+    }
+  
+    this.elRef.nativeElement.querySelectorAll('.scramble')[index].innerText = output;
+  
+    if (complete === this.queues[index].length) {
+      this.resolves[index]();
+    } else {
+      this.frameRequests[index] = requestAnimationFrame(() => this.update(index));
+      this.frame++;
+    }
+  }
+
+  randomChar(): string {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
 }
